0af382861136397c39e017ed77c0ec06579d7729
mads-byte <mbutl224@icloud.com>
2025-10-05 07:36:26 -0400

    add completion message for tax quiz

--- public/learn/learn.js
+++ public/learn/learn.js
@@ -1,8 +1,31 @@
-const arrow2 = document.getElementById('arrow2')
-const lessonDiv2 = document.getElementById('lesson-div2')
-const contentHide2 = document.getElementById('content-hide2')
 const submit = document.getElementById('submit')
 const taxQuiz = document.getElementById('tax-quiz')
+const testResult = document.getElementById('result')
+
+const lessonDiv1 = document.getElementById('lesson-div1')
+const arrow1 = document.getElementById('arrow1')
+const contentHide1 = document.getElementById('content-hide1')
+
+const lessonDiv2 = document.getElementById('lesson-div2')
+const arrow2 = document.getElementById('arrow2')
+const contentHide2 = document.getElementById('content-hide2')
+
+
+const lessonDiv3 = document.getElementById('lesson-div3')
+const arrow3 = document.getElementById('arrow3')
+const contentHide3 = document.getElementById('content-hide3')
+
+const lessonDiv4 = document.getElementById('lesson-div4')
+const arrow4 = document.getElementById('arrow4')
+const contentHide4 = document.getElementById('content-hide4')
+
+const lessonDiv5 = document.getElementById('lesson-div5')
+const arrow5 = document.getElementById('arrow5')
+const contentHide5 = document.getElementById('content-hide5')
+
+const lessonDiv6 = document.getElementById('lesson-div6')
+const arrow6 = document.getElementById('arrow6')
+const contentHide6 = document.getElementById('content-hide6')
 
 
 let lessonsCompleted = ['incomplete', 'incomplete', 'incomplete', 'incomplete', 'incomplete', 'incomplete'];
@@ -29,13 +52,59 @@ function taxQuizScore(obj) {
     return score
 }
 
-arrow2.addEventListener('click', () => {
-    toggleLesson(lessonDiv2, arrow2)
+
+contentHide1.addEventListener('click', () => {
+    toggleLesson(lessonDiv1, arrow1)
 })
 
 contentHide2.addEventListener('click', () => {
     toggleLesson(lessonDiv2, arrow2)
 })
+
+contentHide3.addEventListener('click', () => {
+    toggleLesson(lessonDiv3, arrow3)
+})
+
+contentHide4.addEventListener('click', () => {
+    toggleLesson(lessonDiv4, arrow4)
+})
+
+contentHide5.addEventListener('click', () => {
+    toggleLesson(lessonDiv5, arrow5)
+})
+
+contentHide6.addEventListener('click', () => {
+    toggleLesson(lessonDiv6, arrow6)
+})
+
+
+
+
+arrow1.addEventListener('click', () => {
+    toggleLesson(lessonDiv1, arrow1)
+})
+
+arrow2.addEventListener('click', () => {
+    toggleLesson(lessonDiv2, arrow2)
+})
+
+arrow3.addEventListener('click', () => {
+    toggleLesson(lessonDiv3, arrow3)
+})
+
+arrow4.addEventListener('click', () => {
+    toggleLesson(lessonDiv4, arrow4)
+})
+
+arrow5.addEventListener('click', () => {
+    toggleLesson(lessonDiv5, arrow5)
+})
+
+arrow6.addEventListener('click', () => {
+    toggleLesson(lessonDiv6, arrow6)
+})
+
+
 
 submit.addEventListener('click', (e) => {
     e.preventDefault()
@@ -56,8 +125,11 @@ submit.addEventListener('click', (e) => {
     let finalScore = taxQuizScore(finalQuizData)
     console.log(finalQuizData)
     console.log(taxQuizScore(finalQuizData))
-    if (finalScore === '6/6' || '5/6') {
+    if (finalScore === '6/6' || finalScore === '5/6') {
         lessonsCompleted[1] = 'complete'
+        testResult.innerHTML = `Lesson Complete: You Scored ${finalScore}`
+    } else if (finalScore !== '6/6' || '5/6') {
+        testResult.innerHTML = `Try again: You Scored ${finalScore}`
     }
     console.log(lessonsCompleted)
 })
